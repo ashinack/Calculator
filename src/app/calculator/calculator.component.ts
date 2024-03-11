6b828f0633e0577152aadb72eb44920726d98d50
ashina <ashina@riota.in>
2024-03-11 22:23:54 +0530

    11.03.24

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,15 +7,24 @@ import { Component } from '@angular/core';
 })
 export class CalculatorComponent {
   inputValue: any = '0';
+  operator: any = '';
+  firstValue: any = '';
+  tempValue: any = '';
+  result: any = 0;
 
   getNumber(num: any) {
-    if (this.inputValue != null) {
-      this.inputValue === '0'
-        ? (this.inputValue = num)
-        : (this.inputValue = this.inputValue + num);
-    } else {
+    if (this.inputValue === '0') {
       this.inputValue = num;
+    } else {
+      this.inputValue = this.inputValue + num;
     }
+    // if (this.inputValue != null) {
+    //   this.inputValue === '0'
+    //     ? (this.inputValue = num)
+    //     : (this.inputValue = this.inputValue + num);
+    // } else {
+    //   this.inputValue = num;
+    // }
   }
 
   valueDot() {
@@ -23,8 +32,56 @@ export class CalculatorComponent {
   }
 
   getOperator(operator: any) {
-    if (this.inputValue != null) {
-      this.inputValue = this.inputValue + operator;
+    console.log(this.inputValue, 'this.inputValue');
+    this.tempValue = '';
+
+    this.firstValue = parseFloat(this.inputValue);
+
+    this.operator = operator;
+    this.inputValue = '';
+    // console.log('op', this.operator);
+
+    // if (this.inputValue != null) {
+    //   this.inputValue = this.inputValue + operator;
+    // }
+  }
+
+  evaluate() {
+    this.tempValue = this.inputValue;
+    console.log(this.inputValue, 'op');
+    const a = this.firstValue;
+    const b = parseFloat(this.inputValue);
+    this.result;
+    if (this.operator == '+') {
+      this.result = a + b;
+    }
+    if (this.operator == '*') {
+      this.result = a * b;
+    }
+    if (this.operator == '-') {
+      this.result = a - b;
+    }
+    if (this.operator == '/') {
+      this.result = a / b;
+    }
+    // this.firstValue = result;
+    this.inputValue = this.result.toString();
+
+    console.log('a', a, 'b', b);
+  }
+
+  clearAll() {
+    this.tempValue = '';
+    this.firstValue = '';
+    this.inputValue = '0';
+    this.operator = '';
+  }
+
+  backBtnClicked() {
+    console.log('this.tempValue', this.tempValue);
+    if (this.result == 0) {
+      this.inputValue = this.inputValue.slice(0, this.inputValue.length - 1);
+      console.log('input', this.inputValue);
     }
   }
 }
